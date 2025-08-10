465f6e39a8ccefc3939059c8e5133c47dcfbccc2
Hans Dijkema <hans@dijkewijk.nl>
2025-08-10 11:13:37 +0200

    info

--- info.rkt
+++ info.rkt
@@ -1,14 +1,14 @@
 #lang info
 
 (define pkg-authors '(hnmdijkema))
-(define version "0.1.4")
+(define version "0.1.5")
 (define license 'Apache-2.0)
 (define collection "columns-pane")
-(define pkg-desc "A pane that arranges it's children widgets in columns")
+(define pkg-desc "columns-pane% - A pane that arranges it's children widgets in columns")
 
 (define scribblings
   '(
-    ("scribblings/columns-pane.scrbl" () (gui) "columns-pane")
+    ("scribblings/columns-pane.scrbl" () (gui-library) "columns-pane")
     )
   )
 
